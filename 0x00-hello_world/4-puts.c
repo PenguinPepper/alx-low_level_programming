e3827d73959705903518754cb17f28ab908230e1
Elizabeth <testlolabtest@gmail.com>
2022-09-08 15:37:05 -0700

    Edit file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stidio.h>
+/**
+ * main - Print output using, puts
+ *
+ * Return: the int value of 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
