7e2724df0283e5b9ef66006d9df70a1e3eb2efec
Elizabeth <testlolabtest@gmail.com>
2022-09-08 16:25:47 -0700

    edit file 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio>
+/**
+ * main - Print a string using, printf function
+ *
+ * Return: the int value of void, 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
