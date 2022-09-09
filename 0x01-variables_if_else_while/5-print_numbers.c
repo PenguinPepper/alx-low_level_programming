198447dc56e50ea3863edacf5c4b03567d300a6d
Elizabeth <testlolabtest@gmail.com>
2022-09-09 16:31:26 -0700

    Add file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print 10 digits
+ *
+ * Description: Function will print all numbers of base 10
+ * Return: If all goes well, 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+	printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
