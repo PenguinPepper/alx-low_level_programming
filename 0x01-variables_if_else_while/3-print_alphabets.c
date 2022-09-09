42a382a0e343e833f3521a7383aaa6473b26daf4
Elizabeth <testlolabtest@gmail.com>
2022-09-09 15:43:35 -0700

    Add file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Show combo of lower case and upper case
+ *
+ * Description: Function will print the alphabet in lower and upper case
+ * Return: If all goes well, 0
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a' ; alp <= 'z' ; alp++)
+	{
+		putchar(alp);
+	}
+	for (alp = 'A' ; alp <= 'Z' ; alp++)
+	{
+		putchar(alp);
+	}
+	putchar(alp);
+	return (0);
+}
