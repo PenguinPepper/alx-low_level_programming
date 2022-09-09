fb9f0040fcb399fd994b1287cb51163f1b7de1d2
Elizabeth <testlolabtest@gmail.com>
2022-09-09 14:56:33 -0700

    Add file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+/**
+ * main - Print alphabet in low case
+ *
+ * Description: Print all the letter of alphabet in lower case
+ * Return: If all goes well 0
+ */
+
+int main(void)
+{
+int 0;
+/* your code goes there */
+putchar("a\nb\nc\nd\ne\nf\ng\nh\ni\nj\nk\nl\nm\nn\no\np\nq\nr\ns\nt\nu\nv\nw\nx\ny\nz\n";)
+return (0);
+}
