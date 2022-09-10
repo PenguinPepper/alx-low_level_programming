9067905d8ae00f172046a423682b3bd2c5368f23
Elizabeth <testlolabtest@gmail.com>
2022-09-09 20:21:10 -0700

    Edit file 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+
+/**
+ * main - print numbers of base 12
+ *
+ * Description: Program will print all the numbers of base 16 followed by \n
+ * Return: If all goes well, 0
+ */
+int main(void)
+{
+	int num;
+
+	char alp;
+
+	for (num = '0' ; num <= '9' ; nu++)
+	{
+	putchar(num);
+	}
+	for (alp = 'a' ; alp <= 'f' ; alp++)
+	{
+	putchar(alp);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
