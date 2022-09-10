6bb8a3ae374e3e3483eb9de8a78da45c641df427
Elizabeth <testlolabtest@gmail.com>
2022-09-09 19:52:20 -0700

    Add file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single numbers
+ *
+ * Description: Show all possible combinations of single digit numbers
+ * Return: If all goes well, o
+ */
+int main(void)
+{
+	int combo;
+
+	for (combo = '0' ; combo <= '9' ; combo++)
+	{
+		putchar(combo);
+		if (combo < 9)
+		{
+		putchar(',');
+		putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
