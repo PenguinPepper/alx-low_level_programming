d3136f58f809789efc7a73e772e782c1f64c5b96
Elizabeth <testlolabtest@gmail.com>
2022-09-13 10:24:40 -0700

    Add header file, main.h and the first file, 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print_putchar followed by new line
+ *
+ * Description: Print out _putchar without using the standard libary
+ * Return: If all goes well 0
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	return (0);
+}
