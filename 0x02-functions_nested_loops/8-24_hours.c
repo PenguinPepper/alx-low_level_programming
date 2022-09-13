f2b52c1bb1e43d4035d32cb419d21c4c10510895
Elizabeth <testlolabtest@gmail.com>
2022-09-13 14:23:51 -0700

    Add file 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Description:  prints every minute of the day of Jack Bauer
+ * Return: If all goes well 0
+ */
+void jack_bauer(void)
+{
+
+}
