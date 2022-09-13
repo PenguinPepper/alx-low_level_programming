27938346b9840c0bd1ce74347c166fa520dea739
Elizabeth <testlolabtest@gmail.com>
2022-09-13 12:51:22 -0700

    Add file 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - check ifargument is a character of alphabet
+ * @c: The only argument
+ *
+ * Return: If c is a leter of alphabet regradless of case then 1 otherwise 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' & C <= 'Z')
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
