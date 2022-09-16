2089b1a8bcb5b216cea4b6bdc0d79f6e6b5126cb
Elizabeth <testlolabtest@gmail.com>
2022-09-15 21:32:13 -0700

    Edit file 8 to meet betty check

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
 /**
+ * print_square - hat prints a square, followed by a new line
+ * @size: is the size of the square
  *
+ * Description: prints a square, followed by a new line.
+ * If size is 0 or less, the function should print only a new line.
+ * Use the character # to print the square.
+ * Return: if all goes well 0
  */
 void print_square(int size)
 {
