f08568b894c518846c098f8ef1d36333023202d8
Elizabeth <testlolabtest@gmail.com>
2022-09-15 21:22:28 -0700

    Edit file 6 to meet betty check

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
+ * print_line - draws a straight line in the terminal
+ * @n: the number of times the character _ should be printed
  *
+ * Return: if all goes well 0
  */
 void print_line(int n)
 {
 	 int i;
 
 	 i = 0;
-	 while (i < n)
-	 {
-	 	_putchar('_');
-	 	i++;
-	 }
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
 	_putchar('\n');
 }
