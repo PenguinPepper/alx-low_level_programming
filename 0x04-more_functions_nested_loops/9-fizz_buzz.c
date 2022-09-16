36e17e98d4bf2d3562539cb2442e1255a1b1c41e
Elizabeth <testlolabtest@gmail.com>
2022-09-15 22:25:05 -0700

    Fix syntax error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - print fuzz for prime numbers
  *
- * Return: 0 if all goes well
+ * Return: true if all goes well
  */
+
 int main(void)
 {
+
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
 		}
 		else
 		{
 			printf("%d", i);
 		}
-		if (i < 100)
-		{
-			printf(" ");
-		}
-		printf("\n");
-		return (0);
 	}
+	if (i != 100)
+	{
+		printf(" ");
+
+	}
+	printf("\n");
+	return (0);
 }
