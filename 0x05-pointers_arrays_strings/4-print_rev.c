22d71383f53322c9807aa3db18c5f0bf133a92f6
Elizabeth <testlolabtest@gmail.com>
2022-09-19 03:23:39 -0700

    Add "I can only go one way. I've not got a reverse gear" file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: The string or variable cotaing string
+ *
+ * Return: If all goes well 0
+ */
+void print_rev(char *s)
+{
+}
