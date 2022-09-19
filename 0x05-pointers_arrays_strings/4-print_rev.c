485d4771540886ad6ce3de7b07c6074ec4030e28
Elizabeth <testlolabtest@gmail.com>
2022-09-19 10:59:09 -0700

    Add function statements to file that will reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,4 +8,18 @@
  */
 void print_rev(char *s)
 {
+	int length
+	
+	length = 0;
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+	length--;
+	while (length >= 0)
+	{
+		_putchar(*(s + length));
+		length--;
+	}
+	_putchar('\n');
 }
