df72c3e28c19b5b0f993748cbe75b6102c6ce3b4
Elizabeth <testlolabtest@gmail.com>
2022-09-19 04:07:35 -0700

    Add function file strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src param
+ * @src: string value pointer
+ * @dest: buffer pointer
+ *
+ * Description: Function will copy the string pointed to by src
+ * including the terminating null byte \0 to thr buffer pointed to by dest
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+}
