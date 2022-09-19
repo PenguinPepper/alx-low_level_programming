01628b4beae8b480c9a8e440d783b78660e3aad5
Elizabeth <testlolabtest@gmail.com>
2022-09-19 03:12:44 -0700

    Add function file that will report by its very length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: The stirng or variable containing the string
+ *
+ * Return: If all goes well length of string
+ */
+int _strlen(char *s)
+{
+}
