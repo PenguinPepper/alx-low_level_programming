95359d4287fefe9e693544ae04e5f83766e3f11a
Elizabeth <testlolabtest@gmail.com>
2022-09-19 03:33:55 -0700

    Add the function file "Half the lies they tell about me aren't true"

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * puts2 - will print every character of string starting from the first
+ * @str: String or variable containg string
+ *
+ * Description: Will print every other character of a string, starting with the
+ * first character followed by new line.
+ * Return :If all goes well 0
+ */
+void puts2(char *str)
+{
+}
