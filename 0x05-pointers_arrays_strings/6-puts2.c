f6e6daf708482ad3222b562d8a2d74235f061da1
Elizabeth <testlolabtest@gmail.com>
2022-09-19 11:30:53 -0700

    Add function statement to file that prints every character of string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,4 +10,16 @@
  */
 void puts2(char *str)
 {
+	int index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		if (index % 2 == 0)
+		{
+			_putchar(str[index]);
+		}
+		index++;
+	}
+	_putchar('\n');
 }
