44e21a6e0f964ea389615970fa6bb2a5cc409277
Elizabeth <testlolabtest@gmail.com>
2022-09-19 03:43:44 -0700

    dd function file "Winning is only half of it. Having fun is the other"

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,14 @@
+#include "mai.h"
+
+/**
+ * puts_half - will print half of a string followed by new line
+ * @str: String or variable containing the string
+ *
+ * Description: Function should print the second half of the string
+ * If the number of characters is odd, should print the last n characters of
+ * string, where n = (length_of_the_string - 1) / 2 .
+ * Return: If all goes well 0
+ */
+void puts_half(char *str)
+{
+}
