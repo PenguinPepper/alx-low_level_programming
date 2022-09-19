736a9b37152b67ca4ba58ce1539224abe4e7420f
Elizabeth <testlolabtest@gmail.com>
2022-09-19 03:53:26 -0700

    Add function file arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - will print n elements of an array of integers
+ * @a: Name of array
+ * @n: The number elements of the array to be printed
+ *
+ * Description: Function will print n elements of an array of integers
+ * followed by a new line. Numbers will be seprated by a comma followed by a
+ * space. The numbers will be dispayed in the same order they appear in array
+ * Return: If all goes well 0
+ */
+void print_array(int *a, int n)
+{
+}
