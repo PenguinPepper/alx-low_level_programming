2f7c9be8f0bc05b79b4bf9887081b48deb751706
Elizabeth <testlolabtest@gmail.com>
2022-09-19 03:19:09 -0700

    Add the I do not fear computers. I fear the lack of them file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - print a string followed by a new line to stdout
+ * @str: The string that is taken in as a parameter
+ *
+ * Return: If all goes well 0
+ */
+void _puts(char *str)
+{
+}
