861e1d0644ff239de70ffce53bd8903b56a79d2d
Elizabeth <testlolabtest@gmail.com>
2022-09-19 20:22:11 -0700

    Add function staments to strcpy file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,4 +11,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
+	int fir, sec;
+
+	fir = 0;
+	while (src[fir] != 0)
+	{
+		fir++;
+	}
+	for (sec = 0; sec <= fir; sec++)
+	{
+	dest[sec] = src[sec];
+	}
+	return (dest);
 }
