f241e0c8dd8538bed7544fb9c868a537181bdd39
Elizabeth <testlolabtest@gmail.com>
2022-09-19 20:12:53 -0700

    Add function staement to arrays are not pointers file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,4 +13,16 @@
  */
 void print_array(int *a, int n)
 {
+	int num;
+
+	for (num = 0; num < n; num++)
+	{
+		printf("%d"), a[num];
+		if (num != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
 }
