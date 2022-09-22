f147eb4954d272f8408b6a0a646d37bbf093a2a7
Elizabeth <testlolabtest@gmail.com>
2022-09-22 15:30:42 -0700

    Fix syntax in 6. Expect the best. Prepare for the worst... file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,25 +11,25 @@ char *cap_string(char *str)
 	int normchar = 0, i;
 	int except[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + normchar) >= 97 && *(s + normchar) <= 122)
+	if (*(str + normchar) >= 97 && *(str + normchar) <= 122)
 	{
-		*(s + normchar) = *(s + normchar) - 32;
+		*(str + normchar) = *(str + normchar) - 32;
 	}
 	normchar++;
-	while (*(s + normchar) != '\0')
+	while (*(str + normchar) != '\0')
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + normchar) == except[i])
+			if (*(str + normchar) == except[i])
 			{
-				if ((*(s + (normchar + 1)) >= 97) && (*(s + (normchar + 1)) <= 122))
+				if ((*(str + (normchar + 1)) >= 97) && (*(str + (normchar + 1)) <= 122))
 				{
-					*(s + (normchar + 1)) = *(s + (norchar + 1)) - 32;
+					*(str + (normchar + 1)) = *(str + (norchar + 1)) - 32;
 					break;
 				}
 			}
 		}
 		normchar++;
 	}
-	return (s);
+	return (str);
 }
