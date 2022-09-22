d97933ca3b42a88a0b967d665c25beb914751649
Elizabeth <testlolabtest@gmail.com>
2022-09-22 10:46:34 -0700

    Add 2. strncpy file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: variable containg first char argument
+ * @src: variable containing sec argument or string variable
+ * @n: third argumentto be given as number
+ *
+ * Description: copies a string and works exaclty like strncpy function
+ * Return: IF all goes well 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	do {
+		dest[i] = src[i];
+		i++;
+	} while (src[i] != '\0')
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
