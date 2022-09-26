e4c408964978e25ccbd6ff3ace254809e1e817dc
Elizabeth <testlolabtest@gmail.com>
2022-09-26 00:07:15 -0700

    Add 6. Chess is mental torture function file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_chessboard -  prints the chessboard
+ * @a: variable containg 2D array
+ *
+ * Return: if all goes well 0
+ */
+void print_chessboard(char (*a)[8])
+{
+}
