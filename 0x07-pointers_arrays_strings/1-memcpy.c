ab95d6cd1f1ac037fa034aebdedac99f4cdc48a5
Elizabeth <testlolabtest@gmail.com>
2022-09-25 23:37:00 -0700

    Add 1. memcpy function file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @n: Unknown bytes to be copied
+ * @src: First ememory area
+ * @dest: Second memory area
+ *
+ * Description: Copies n bytes from memory area src to memory area dest
+ * Return: Pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+}
