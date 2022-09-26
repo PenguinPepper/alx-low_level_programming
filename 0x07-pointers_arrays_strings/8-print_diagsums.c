b49f70833e2354df79971d6a5f96ef04168de36f
Elizabeth <testlolabtest@gmail.com>
2022-09-26 13:28:08 -0700

    Fix errors

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,7 +11,7 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, int j, sum1 = 0, sum2 = 0;
+	int i, j, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
@@ -22,5 +22,4 @@ void print_diagsums(int *a, int size)
 		sum2 = a[i] + ((size * i) + 1);
 	}
 	printf("%i + %i\n", sum1, sum2);
-	return (0);
 }
