7e86bf6603da818e124e25404ccea578fee9e2cb
Elizabeth <testlolabtest@gmail.com>
2022-09-26 13:25:23 -0700

    Use different staement

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,5 +11,16 @@
  */
 void print_diagsums(int *a, int size)
 {
+	int i, int j, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 = a[i] + ((size * i) + 1);
+	}
+	for (j = 0; j < size; j++)
+	{
+		sum2 = a[i] + ((size * i) + 1);
+	}
+	printf("%i + %i\n", sum1, sum2);
 	return (0);
 }
