194ef7440dae9b12526e3f072a2bff74162fa598
Elizabeth <testlolabtest@gmail.com>
2022-09-28 00:18:45 -0700

    Add 6. Inception. Is it possible? file

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Retuens 1 if input is a prime number otherwise return 0
+ * @n: inter ger to be checked if it is a prime number
+ *
+ * Return: 1 if primenumber else return 0 if not prime number
+ */
+int is_prime_number(int n)
+{
+}
