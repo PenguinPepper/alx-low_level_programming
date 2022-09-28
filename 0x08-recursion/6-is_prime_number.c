13465e5de1f1a28e7bc64fb7ca3a28e87eec677c
Elizabeth <testlolabtest@gmail.com>
2022-09-28 14:55:51 -0700

    Add function statement to 6. Inception. Is it possible? file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * are_you - checks if you(number) are prime
+ * @n: the number to be checked
+ * @i: second param
+ *
+ * Return: 1 for prime number otherwise 0
+ */
+int are_you(int n, int i)
+{
+	if ((n <= 1) || (n % i == 0 && i > 1))
+		return (0);
+	if ((n / i) < i)
+		return (1);
+	return (are_you(n, i + 1));
+}
+
+
+/**
  * is_prime_number - Retuens 1 if input is a prime number otherwise return 0
  * @n: inter ger to be checked if it is a prime number
  *
@@ -8,4 +25,5 @@
  */
 int is_prime_number(int n)
 {
+	return (are_you(n, 1));
 }
