99e25ddc826cfdd84cb4f5c54354c85f419ed27e
Elizabeth <testlolabtest@gmail.com>
2022-09-28 00:07:59 -0700

    Add 4. Once an idea has taken hold of the brain... file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function will recturn the value of x raised to e power of y
+ * @x: number to be multipled y number of times
+ * @y: number to raise to the power of
+ *
+ * Description: eturns the value of x raised to the power of y
+ * If y is lower than 0, the function should return -1
+ * Return: x to power of y
+ */
+int _pow_recursion(int x, int y)
+{
+}
