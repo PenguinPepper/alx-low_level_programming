aa7741875ff5202129c215331856f2ab58720e1a
Elizabeth <testlolabtest@gmail.com>
2022-09-30 15:22:18 -0700

    Edit 3. Neither irony nor sarcasm is argument file to meet betty style

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,7 +3,15 @@
 #include "main.h"
 
 /**
+ * main - program that multiplies two numbers
+ * @argc: count of args present
+ * @argv: array of char * pointing to args
  *
+ * Description: print the result of the multiplication, followed by a new line
+ *  two numbers and result of the multiplication can be stored in an integer
+ *  If the program does not receive two arguments, your program
+ *  should print Error, followed by a new line, and return 1
+ *  Return: if all goes well 0
  */
 int main(int argc, char *argv[])
 {
