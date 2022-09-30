bfe5f7bce5a0644640369b27ea3eb9bf14034335
Elizabeth <testlolabtest@gmail.com>
2022-09-30 15:27:01 -0700

    Edit 4. To infinity and beyond... file to meet betty style

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,7 +3,16 @@
 #include <stdlib.h>
 
 /**
+ * main - program that adds positive numbers
+ * @argc:  count of args present
+ * @argv: array of char * pointing to args
  *
+ *  Description: Print the result, followed by a new line If no number
+ *   is passed to the program, print 0, followed by a new line
+ *   If one of the number contains symbols that are not digits, print Error
+ *   followed by a new line, and return 1 numbers and the addition of all
+ *   the numbers can be stored in an int
+ *   Return: If all goes well 0
  */
 int main(int argc, char *argv[])
 {
