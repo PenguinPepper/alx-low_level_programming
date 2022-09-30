1c3314344ce1301ef9125877b79a6472909f1600
Elizabeth <testlolabtest@gmail.com>
2022-09-30 15:14:59 -0700

    Edit 1. Silence is argument... file to betty style

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 /**
+ * main - program that prints the number of arguments passed into it
+ * @argc: count of args present
+ * @argv: array of char * pointing to args
  *
+ * Return: if all goes well 0
  */
 int main(int argc, char *argv[])
 {
