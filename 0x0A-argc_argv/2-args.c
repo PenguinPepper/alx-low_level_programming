880db0f817c27e2b95af2e33f9a27586d5471cd1
Elizabeth <testlolabtest@gmail.com>
2022-09-30 15:18:03 -0700

    Edit 2. The best argument... file to meet betty style

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - program that prints all arguments it receives
+ * @argc:count of args present
+ * @argv: array of char * pointing to args
  *
+ *  Description: All arguments should be printed, including the first one
+ *  Only print one argument per line, ending with a new line
+ *  Return: if all goes well 0
  */
 int main(int argc, char *argv[])
 {
