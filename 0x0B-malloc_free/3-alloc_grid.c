6e15186c997db4ae0720ed2b59864df9e47873b3
Elizabeth <testlolabtest@gmail.com>
2022-10-04 10:40:45 -0700

    Add files 2, 3 and 4

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: The width of the 2D array
+ * @height: The height of the 2D array
+ *
+ * Description: Each element of the grid should be initialized to 0.
+ * The function should return NULL on failure.
+ * If width or height is 0 or negative, return NULL
+ * Return: 2D array
+ */
+int **alloc_grid(int width, int height)
+{
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * previously created by your alloc_grid function.
+ * @grid: grid to be freed
+ * @height: height of an array
+ *
+ * Return: if all goes well 0
+ */
+void free_grid(int **grid, int height)
+{
+}
