d0a456d8f271c091ddeee8a87364705703a1e0ee
Elizabeth <testlolabtest@gmail.com>
2022-10-06 14:30:07 -0700

    Fix err in file 1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -40,7 +40,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < length1; i++)
 		ptr[i] = s1[i];
 	for (j = 0; j < n; j++)
-		ptr[length1 + j] = s2 [j];
+		ptr[length1 + j] = s2[j];
 	ptr[length1 + j] = '\0';
 	if (ptr)
 		return (ptr);
