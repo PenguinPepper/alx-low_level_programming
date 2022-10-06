62fdf8a9253f7110edfefcb2d5993385e2060236
Elizabeth <testlolabtest@gmail.com>
2022-10-06 14:21:13 -0700

    Correct statemeents in 1. string_nconcat... file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,5 +16,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j, 
+	unsigned int i, j, length1, length2;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	length1 = 0;
+	while (s1[length1] != '\0')
+		length1++;
+	while (s2[length2] != '\0')
+		length2++;
+	if (n >= length2)
+		n = length2;
+
+	ptr = malloc(sizeof(char) * (length1 + n + 1));
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i < length1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < n; j++)
+		ptr[length1 + j] = s2 [j];
+	ptr[length1 + j] = '\0';
+	if (ptr)
+		return (ptr);
+	return (NULL);
+
 }
