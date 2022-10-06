7c29b55f6b5b09c0b7587acdf24aa97a811f6cae
Elizabeth <testlolabtest@gmail.com>
2022-10-06 14:40:14 -0700

    Edit function staement in file 2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,17 +13,19 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i = 0;
-	char *ptr = malloc(nmemb * size);
+	char *pr;
+	void *plh;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-
+	plh =ptr;
 	while (i < nmemb * size)
 	{
 		ptr[i] = 0;
 		i++;
 	}
-	return (ptr);
+	return (plh);
 }
