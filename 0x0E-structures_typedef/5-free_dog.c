ae7bed1dfa5843fb9312fdf5a658a0224ce048e8
Elizabeth <testlolabtest@gmail.com>
2022-10-10 13:58:42 -0700

    Add function to free statement in 5. How many legs... file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs
+ * @d: name of the new type dog_t
+ *
+ * Return: if all goes well 0
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
