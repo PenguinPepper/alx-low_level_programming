7797cb749574afb043065153b3992d720fda0520
Elizabeth <testlolabtest@gmail.com>
2022-10-12 15:28:20 -0700

    Edit to include exit status

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,6 +17,7 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
+		exit(98);
 	}
 
 	num1 = atoi(argv[1]);
@@ -26,11 +27,13 @@ int main(int argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
+		exit(99);
 	}
 
 	if ((*op == '%' && num2 == 0) || (*op == '/' && num2 == 0))
 	{
 		printf("Error\n");
+		exit(100);
 	}
 
 	printf("%d\n", get_op_func(op)(num1, num2));
