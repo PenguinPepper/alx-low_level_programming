327347d045d64881c7946b4f5580df5a595aa4ca
Elizabeth <testlolabtest@gmail.com>
2022-10-12 13:00:16 -0700

    Edit file 1-array... to add for conditions to if statement

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,8 +1,11 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * array_iterator - executes a function given as a
  * parameter on each element of an array
+ * @array: array to be searched
  * @size: is the size of the array
  * @action: a pointer to the function you need to use
  *
@@ -12,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (array && action)
+	if (array != NULL && action != NULL && size > 0)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
