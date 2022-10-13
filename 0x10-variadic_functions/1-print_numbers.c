472df7de7d4ca02a2ef7734a567710d5a7257bb4
Elizabeth <testlolabtest@gmail.com>
2022-10-13 12:33:42 -0700

    Fix spelling errors in statements

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(nums, int));
 		if (separator && i != n -1)
-			printf("%s", seperator);
+			printf("%s", separator);
 	}
 
 	va_end(nums);
