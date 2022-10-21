4626dd905c07657f3bfac0336b5b5f98dcfee5d1
Elizabeth <testlolabtest@gmail.com>
2022-10-20 17:29:02 -0700

    Add 0. Print list... file with function definition

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,13 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to next elements in list
+ *
+ * Description: If str is NULL, print [0] (nil)
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+}
