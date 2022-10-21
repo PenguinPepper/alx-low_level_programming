2ab2dba097550a8160dc8a7dba66af3c420f9d6f
Elizabeth <testlolabtest@gmail.com>
2022-10-20 17:43:02 -0700

    Add function statements to function to print elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,4 +10,17 @@
  */
 size_t print_list(const list_t *h)
 {
+	size_t size = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->length, h->str);
+
+		h = h->next;
+		size++;
+	}
+	return (size);
 }
