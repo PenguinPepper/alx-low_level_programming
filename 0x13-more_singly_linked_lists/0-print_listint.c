45acbb7c3df113530864742ba3ca0f3445fc9b3d
Elizabeth <testlolabtest@gmail.com>
2022-10-25 13:31:17 -0700

    Add function statements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+#include <stddef.h>
+
+/**
+ * print_listint - a function that prints all the elements of a listsint_t list
+ * @h: new node
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const list_int *h)
+{
+	size_t length = 0;
+
+	while(h != NULL)
+	{
+		printf("\n%d", h->n);
+		h = h->next;
+		length++;
+	}
+	return (length);
+}
