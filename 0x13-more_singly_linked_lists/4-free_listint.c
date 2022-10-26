5d987f14f03bf1c3927ef7245346dbc6752d38c6
Elizabeth <testlolabtest@gmail.com>
2022-10-26 14:46:14 -0700

    Add function statements to freethe list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,22 @@
 #include "lists.h"
+
+/**
+ * ree_listint - function that frees a listint_t list
+ * @head: pointer to start of list
+ *
+ * Return: if alll goes well 0
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+	head = NULL;
+}
