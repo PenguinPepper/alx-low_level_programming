736e6cc515f4d147c49e5c4fdfffd893ea172de7
Elizabeth <testlolabtest@gmail.com>
2022-10-26 15:40:31 -0700

    Add function statements to add all node data

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,23 @@
 #include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data (n) of a listint_t linked list
+ * @head: pointer to list
+ *
+ * Return: if list is empty 0, else return sum
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *nowItem;
+	size_t sum = 0;
+
+	if (!head)
+		return (0);
+	nowItem = head;
+	while (nowItem)
+	{
+		sum += nowItem->n;
+		nowItem = nowItem->next;
+	}
+	return (sum);
+}
