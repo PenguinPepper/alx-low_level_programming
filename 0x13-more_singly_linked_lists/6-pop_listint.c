1b86f429031b91f2fc2619b2bac459296a751378
Elizabeth <testlolabtest@gmail.com>
2022-10-26 15:17:47 -0700

    Add function statements to delete head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,1 +1,22 @@
 #include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t
+ * linked list, and returns the head node’s data (n)
+ * 2head: ponter to list
+ *
+ * Return: if empty 0, else return node data n
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *nowItem;
+	int left;
+
+	if (!(*head) || !head)
+		return (0);
+	nowItem = *head;
+	left = nowItem->n;
+	*head = (*head)->next;
+	free(nowItem);
+	return (left);
+}
