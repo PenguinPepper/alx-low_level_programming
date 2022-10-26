1c6ceccbaa630fd9a110f4bcae2a959e74e693eb
Elizabeth <testlolabtest@gmail.com>
2022-10-26 15:08:22 -0700

    Add function statements to 5. Free... file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,26 @@
 #include "lists.h"
+
+/**
+ * free_listint2 - function that frees a
+ * listint_t list and sets the head to NULL
+ * @head: pointer to list
+ *
+ * Return: if all goes well 0
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *nowItem;
+	listint_t *tmp;
+
+	if (!head)
+		return;
+	nowItem = *head;
+	while (nowItem)
+	{
+		tmp = nowItem;
+		nowItem = tmp->next;
+		free(tmp);
+	}
+	*head = NULL;
+	head = NULL;
+}
