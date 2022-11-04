ae429aa7f0fbc2db4e0096da1226a6dc7189cb04
Elizabeth <testlolabtest@gmail.com>
2022-11-04 08:35:24 -0700

    Edit file to meet betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,12 +18,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for(i = 0; text_content[i]; i++)
+		for (i = 0; text_content[i]; i++)
 			;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC);
-	w = write(o,  text_content, i);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, i);
 
 	if (o == -1 || w == -1)
 		return (-1);
