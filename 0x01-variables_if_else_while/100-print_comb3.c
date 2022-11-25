3cbf76b7df3fefa81e59cb5946565272c47cb6f6
Elizabeth <testlolabtest@gmail.com>
2022-11-25 01:01:16 -0800

    Add fuction that prints all possible combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: if all goes well 0
+ */
+int main(void)
+{
+	char i, j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			if (j == i || j < i)
+				continue;
+			if (j > 49 && j <= 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar(i);
+			putchar(j);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
