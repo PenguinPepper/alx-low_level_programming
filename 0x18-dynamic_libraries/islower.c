f590f9874ea516e4d2a8f72018927b86cbba61b9
Elizabeth <testlolabtest@gmail.com>
2022-12-16 01:17:49 -0800

    Add dynamic library

---
+++ 0x18-dynamic_libraries/islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - check for lower case characters
+ * @c: The only agrument
+ *
+ * Return: 1 if c is lowercase and 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	else
+		return (0);
+}
