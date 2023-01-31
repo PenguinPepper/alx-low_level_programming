1371bd490650e2a0aa2a1b8e124681b96744a3d3
Elizabeth <testlolabtest@gmail.com>
2023-01-31 13:35:57 -0800

    Correctt doc according to betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
  * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: head of linked list.
  *
  * Return: number of elements in a linked list.
  */
