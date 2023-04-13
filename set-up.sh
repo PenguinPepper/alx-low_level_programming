eaaefb3c35b9d5e3b58beac71a2de9153c22660d
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-13 03:47:13 -0700

    Edit the file to pass shellcheck

--- set-up.sh
+++ set-up.sh
@@ -1,6 +1,6 @@
 #!/usr/env/bash
 # Bash script that creates a directory and a readme for the directory
-mkdir $1 && cd $1
-echo "$2"> README.md
+mkdir "$1" && cd "$1" || exit
+echo "#$2"> README.md
 echo "This directory contains the files for ALX project $2">>README.md
-vim $3
+vim "$3"
