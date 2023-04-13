6d2366506007c38dd40adac76876a438911cbd1c
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-13 03:54:34 -0700

    Edit set-up.sh and delete the trial directory

--- set-up.sh
+++ set-up.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Bash script that creates a directory and a readme for the directory
 mkdir "$1" && cd "$1" || exit
-echo "#$2"> README.md
+echo "# $2\n"> README.md
 echo "This directory contains the files for ALX project $2">>README.md
-vim "$3"
+cd "$1" vim "$3"
