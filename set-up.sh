29d343e977b3531852a58208be656b2e2c377224
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-13 04:06:33 -0700

    Edit set-up script

--- set-up.sh
+++ set-up.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Bash script that creates a directory and a readme for the directory
 mkdir "$1" && cd "$1" || exit
-echo "# $2\n"> README.md
+printf "# %s\n" "$2" > README.md
 echo "This directory contains the files for ALX project $2">>README.md
-cd "$1" vim "$3"
+cd "$1" && vim "$3" || exit
