833951b20cafbeb82a4199e883e3e8b6acb46aea
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-14 06:02:32 -0700

    Add function that will perform a linear search on an array of size n

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,3 +1,5 @@
+#include "search_algos.h"
+
 /**
  * linear_search - function that searches for a value in
  * an array of integers using the linear search algorithm
@@ -7,6 +9,15 @@
  *
  * Return: first index where value is located
  */
-int linear_search(int *array, size_t size, int value){
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
 
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+	}
+	return (-1);
 }
