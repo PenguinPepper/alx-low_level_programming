93c8624940b88a1232010c353483c743881cdfef
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-05-09 16:48:29 -0700

    Add file for exponetial search

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+/**
+ * exponential_search - function that searches for a value in a
+ * sorted array of integers using the exponential search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: the index where value is located
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+}
