7ef14194a00098600d1871f2e442d02e1fe5981b
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-05-10 14:28:53 -0700

    Make output be correct

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,4 +10,20 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t low = 0, high = size - 1, mid;
+
+	if (array == NULL)
+		return -1;
+	while (low < high)
+	{
+		mid = low + (high -low) / 2;
+		printf("Searching in array: %d", array[i]);
+		if (value == array[mid])
+			return (mid);
+		else if (value < array[mid])
+			high = mid -1;
+		else 
+			low = mid + 1;
+	}
+	return -1;
 }
