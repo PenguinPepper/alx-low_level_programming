de15df45c209516e260180bfb6af83b177572b78
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-05-10 16:00:26 -0700

    Write the iterative implementation of a binary search algorithm on an array

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,20 +10,27 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1, mid;
+	size_t low = 0, i, high = size - 1, mid;
 
 	if (array == NULL)
-		return -1;
+		return (-1);
 	while (low < high)
 	{
-		mid = low + (high -low) / 2;
-		printf("Searching in array: %d", array[i]);
+		mid = low + (high - low) / 2;
+
+		printf("Searching in array: %d", array[low]);
+		for (i = low + 1; i <= high; i++)
+		{
+			printf(", %d", array[i]);
+		}
+		printf("\n");
+
 		if (value == array[mid])
 			return (mid);
 		else if (value < array[mid])
-			high = mid -1;
-		else 
+			high = mid - 1;
+		else
 			low = mid + 1;
 	}
-	return -1;
+	return (-1);
 }
